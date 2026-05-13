7abfdd7cfa6dd382acef9ccf2f5ae28808e7f3fe
ngodongkc123-lgtm <ngodongkc123@gmail.com>
2026-05-13 16:34:57 +0700

    Update Rank.lua

--- Rank.lua
+++ Rank.lua
@@ -1,12 +1,12 @@
 -- DANH SÁCH SCRIPT THEO TỪ KHÓA (giữ nguyên mẫu)
 local questScripts = {
-    ["coin jar"]  = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/CoinJar.lua",
-    ["comet"]     = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/Comet.lua",
-    ["lucky block"] = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/LuckyBlock.lua",
-    ["pinata"]    = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/Pinata.lua",
-    ["egg"]       = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/Egg.lua",
-    ["gold"]      = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/Gold.lua",
-    ["rainbow"]   = "https://raw.githubusercontent.com/quocngo1412/Rank/refs/heads/main/Rainbow.lua"
+    ["coin jar"]  = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/CoinJar.lua",
+    ["comet"]     = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/Comet.lua",
+    ["lucky block"] = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/LuckyBlock.lua",
+    ["pinata"]    = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/Pinata.lua",
+    ["egg"]       = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/Egg.lua",
+    ["gold"]      = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/Gold.lua",
+    ["rainbow"]   = "https://raw.githubusercontent.com/ngodongkc123-lgtm/rank2.lua/refs/heads/main/Rainbow.lua"
 }
 
 -- ==================== TẠO UI ====================
